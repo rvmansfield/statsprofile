a2bacf8ff41a9230ba598d29705fe5145fa9c0ed
rvmansfield <rvmansfield@gmail.com>
2026-01-17 06:37:17 -0500

    static root change

--- statsprofile/settings.py
+++ statsprofile/settings.py
@@ -160,10 +160,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
